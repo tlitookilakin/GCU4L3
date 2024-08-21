74b82a7cff59fe17c565c095264262a0af05fdf6
Afseen Salam <afseenfazil@gmail.com>
2024-08-21 15:12:48 -0400

    Created the Car class

---
+++ Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLab
+{
+    internal class Car
+    {
+        //properties
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+
+        //Constructor
+        public Car()
+        {
+            Make = "";
+            Model = "";
+            Year = 0;
+            Price = 0;
+
+        }
+
+        public Car(string _make,string _model,int _year,decimal _price)
+        {
+            Make = _make;
+            Model = _model;
+            Year = _year;
+            Price = _price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Make}\t{Model}\t{Year}\t{Price}";
+        }
+
+    }
+}
